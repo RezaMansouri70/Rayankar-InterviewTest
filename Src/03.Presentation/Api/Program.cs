73eab0f4b6b949f410fa03421899596a27f7b11d
Reza <reza.mansouri70@gmail.com>
2022-08-07 17:36:27 +0430

    Change  Program.cs In Presentation Layer Api project Add Transient Validator Class For Create Edit And Delete Customer Add DbContext and EventStoreDbContext Add Swagger For Test In API Add EventLoggerBehavior And etc

--- Src/03.Presentation/Api/Program.cs
+++ Src/03.Presentation/Api/Program.cs
@@ -1,11 +1,44 @@
+
+using Api.Behaviours;
+using Api.Middlewares;
+using ApplicationServices.Customer.Commands.CreateCustomer;
+using ApplicationServices.Customer.Commands.DeleteCustomer;
+using ApplicationServices.Customer.Commands.EditCustomer;
+using ApplicationServices.Customer.Queries.GetCustomerById;
+using ApplicationServices.Mapper;
+using DataLayer.SqlServer.Common;
+using DataLayer.SqlServer.EventStore;
+using FluentValidation;
+using FluentValidation.AspNetCore;
+using MediatR;
+using Microsoft.EntityFrameworkCore;
+
 var builder = WebApplication.CreateBuilder(args);
 
-// Add services to the container.
 
-builder.Services.AddControllers();
+builder.Services.AddControllers().AddFluentValidation();
+
+builder.Services.AddTransient<IValidator<CreateCustomerCommand>, CreateCustomerCommandValidator>();
+builder.Services.AddTransient<IValidator<EditCustomerCommand>, EditCustomerCommandValidator>();
+builder.Services.AddTransient<IValidator<DeleteCustomerCommand>, DeleteCustomerCommandValidator>();
+
+builder.Services.AddMediatR(typeof(CreateCustomerCommandHandler));
+builder.Services.AddMediatR(typeof(GetCustomerByIdQueryHandler));
+
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
+
+var cnnString = builder.Configuration.GetConnectionString("AppCnn");
+builder.Services.AddDbContext<ApplicationDbContext>(options => options.UseSqlServer(cnnString));
+builder.Services.AddAutoMapper(typeof(DomainProfile).Assembly);
+
+builder.Services.AddSingleton<IEventStoreDbContext, EventStoreDbContext>();
+
+builder.Services.AddScoped(typeof(IPipelineBehavior<,>), typeof(EventLoggerBehavior<,>));
+
+
+
 
 var app = builder.Build();
 
@@ -15,6 +48,7 @@ if (app.Environment.IsDevelopment())
     app.UseSwagger();
     app.UseSwaggerUI();
 }
+app.UseMiddleware<ErrorHandlingMiddleware>();
 
 app.UseHttpsRedirection();
 
