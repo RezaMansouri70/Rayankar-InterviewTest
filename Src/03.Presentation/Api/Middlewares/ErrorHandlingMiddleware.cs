06e36f3548e09ca545018f68277e1b2913f86306
Reza <reza.mansouri70@gmail.com>
2022-08-07 17:25:09 +0430

    Add Middlewares For log error by logger

---
+++ Src/03.Presentation/Api/Middlewares/ErrorHandlingMiddleware.cs
@@ -0,0 +1,43 @@
+﻿using Newtonsoft.Json;
+
+namespace Api.Middlewares
+{
+    public class ErrorHandlingMiddleware
+    {
+        private readonly RequestDelegate _next;
+        private readonly ILogger<ErrorHandlingMiddleware> _logger;
+
+        public ErrorHandlingMiddleware(RequestDelegate next,
+            ILogger<ErrorHandlingMiddleware> logger)
+        {
+            _next = next;
+            _logger = logger;
+        }
+
+        public async Task Invoke(HttpContext context)
+        {
+            try
+            {
+                await _next(context);
+            }
+            catch (Exception ex)
+            {
+                await HandleExceptionAsync(context, ex);
+            }
+        }
+
+        private async Task HandleExceptionAsync(HttpContext context, Exception exception)
+        {
+            object? errors = default;
+            errors = exception.Message;
+            _logger.LogError($"{errors} - {exception.Source} - {exception.Message} - {exception.StackTrace} - {exception.TargetSite?.Name}");
+
+            context.Response.ContentType = "application/json";
+
+            await context.Response.WriteAsync(JsonConvert.SerializeObject(new
+            {
+                errors
+            }));
+        }
+    }
+}
